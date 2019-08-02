059e7a9f13cb09b30d90db8aae519efe4ab719ca
tmurvv <tmurv@shaw.ca>
2019-08-02 09:15:12 -0600

    click on passenger calls reward

--- src/components/GetPassengers.js
+++ src/components/GetPassengers.js
@@ -9,8 +9,13 @@ class GetPassengers extends Component {
     }
     handleClick() {
         console.log('click');
-        //document.querySelector('#reward').style.display = 'block';
-
+        document.querySelector('#reward').style.display = 'block';
+        document.querySelector('#amount').style.transform = 'scale(2.0)';
+        setTimeout(() => {
+            document.querySelector('#reward').style.display = 'none';
+            document.querySelector('#amount').style.transform = 'scale(1.0)';
+            window.location.reload();
+        }, 4000);
     }
     render() { 
         return (            
@@ -21,21 +26,21 @@ class GetPassengers extends Component {
                 <div className="gameover">GAME OVER</div>
                 <h1>CATCH THE PASSENGER ! <span>(click on the person)</span></h1>
                 <h2>+ $10.00</h2>
-                <input class="input-circle input-circle1" type="radio" id="circle1" onClick={this.handleClick}/>
+                <input className="input-circle input-circle1" type="radio" id="circle1" onClick={this.handleClick}/>
                 {/* <input class="input-circle input-circle2" type="radio" id="circle2" />
                 <input class="input-circle input-circle3" type="radio" id="circle3" />
                 <input class="input-circle input-circle4" type="radio" id="circle4" />
                 <input class="input-circle input-circle5" type="radio" id="circle5" />
                 <input class="input-circle input-circle6" type="radio" id="circle6" /> */}
 
-                <label HTMLfor="circle1" class="pajaro pajaro1"><span></span></label>
+                <label htmlFor="circle1" className="pajaro pajaro1"><span></span></label>
                 {/* <label for="circle2" class="pajaro pajaro2"><span></span></label>
                 <label for="circle3" class="pajaro pajaro3"><span></span></label>
                 <label for="circle4" class="pajaro pajaro4"><span></span></label>
                 <label for="circle5" class="pajaro pajaro5"><span></span></label>
                 <label for="circle6" class="pajaro pajaro6"><span></span></label> */}
                 <div className="reward" id="reward"></div>
-                <div className="sum">SCORE:</div>
+                <div className="sum" id='amount'>+ $10.0</div>
                 <footer><a href="https://il.linkedin.com/in/eladshechter/">The Game Done By Elad Shechter</a></footer>
             </div>
          );
